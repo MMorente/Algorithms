08175ac858bb63fb791ad7371bd69751eee6bd8b
DESKTOP-IRDUAHB\usuario <morenteluna@gmail.com>
2021-12-08 14:48:32 +0100

    Add project files.

---
+++ Test/Test.h
@@ -0,0 +1,24 @@
+﻿// Test.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <queue>
+#include <map>
+#include <vector>
+
+class ShortestPath {
+public:
+	ShortestPath(const std::vector<std::vector<int>>& m);
+	bool is_valid(int row, int col);
+	int solve();
+
+private:
+	std::vector<std::vector<int>> matrix;
+	const int rows;
+	const int cols;
+	int rowNum[4] = { -1, 0, 0, 1 };
+	int colNum[4] = { 0, -1, 1, 0 };
+
+};
---
+++ Test/main.cpp
@@ -0,0 +1,11 @@
+#include "Test.h"
+
+
+int main(int argc, char* argv[]) {
+	std::vector<std::vector<int>> test{ {1,1,1,1,1},{0,1,1,1,0},{1,1,0,1,1},{1,1,1,1,9} };
+
+	ShortestPath solver(test);
+	int solution = solver.solve();
+
+	std::cout << "The shortest path for the given 2D matrix is " << solution << " jumps.\n";
+}
